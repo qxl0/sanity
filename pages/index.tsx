f4c6527855851377cc24a53e82d817042134f4ef
qiang li <qxl0@yahoo.com>
2022-02-28 11:44:20 -0500

    Coding the top part after Header

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,21 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
+
+      <div>
+        <div>
+          <h1 className="max-w-xl font-serif text-6xl">
+            <span className="underline decoration-black">Medium</span> is a
+            place to write, read, and connect
+          </h1>
+          <h2>
+            It's easy and free to post your thinking on any topic and connect
+            with millions of readers.
+          </h2>
+        </div>
+
+        <div></div>
+      </div>
     </div>
   )
 }
