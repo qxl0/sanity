b63fffcd0ffffb957adf003de480f22feb0261e7
qiang li <qxl0@yahoo.com>
2022-02-28 19:46:11 -0500

    Adding page query

--- sanity.js
+++ sanity.js
@@ -10,3 +10,9 @@ export const config = {
   apiVersion: '2021-03-25',
   useCdn: process.env.NODE_ENV === 'production',
 }
+
+export const sanityClient = createClient(config)
+
+export const urlFor = (source) => createImageUrlBuilder(config).image(source)
+
+export const useCurrentUser = createCurrentUserHook(config)
