928d29f87ddeec97d41a878cbb81822ee523affd
qiang li <qxl0@yahoo.com>
2022-03-01 19:55:04 -0500

    Set up hover animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -42,8 +42,12 @@ const Home = ({ posts }: Props) => {
       <div className="grid grid-cols-1 gap-3 p-2 sm:grid-cols-2 md:gap-6 md:p-6 lg:grid-cols-3">
         {posts.map((post) => (
           <Link key={post._id} href={`/post/${post.slug.current}`}>
-            <div>
-              <img src={urlFor(post.mainImage).url()!} alt="" />
+            <div className="group">
+              <img
+                className="h-60 w-full object-cover group-hover:scale-105"
+                src={urlFor(post.mainImage).url()!}
+                alt=""
+              />
               <div className="flex justify-between bg-white p-5">
                 <div>
                   <p>{post.title}</p>
