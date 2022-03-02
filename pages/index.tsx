ef0d9a9f34033ccf56095487583d4004f6a60e85
qiang li <qxl0@yahoo.com>
2022-03-01 19:58:56 -0500

    Added border, overflow-hidden

--- pages/index.tsx
+++ pages/index.tsx
@@ -42,9 +42,9 @@ const Home = ({ posts }: Props) => {
       <div className="grid grid-cols-1 gap-3 p-2 sm:grid-cols-2 md:gap-6 md:p-6 lg:grid-cols-3">
         {posts.map((post) => (
           <Link key={post._id} href={`/post/${post.slug.current}`}>
-            <div className="group">
+            <div className="group cursor-pointer overflow-hidden rounded-lg border">
               <img
-                className="h-60 w-full object-cover group-hover:scale-105"
+                className="h-60 w-full object-cover transition-transform duration-200 ease-in-out group-hover:scale-105"
                 src={urlFor(post.mainImage).url()!}
                 alt=""
               />
